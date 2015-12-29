78c15789f47be808dd14cb7eb626b9b88e4fedaf
Backburn <backburn@hotmail.com>
2015-12-28 20:57:17 -0800

    Marksmanship Sniper update

--- Routines/Advanced/Sniper/Marksmanship.cs
+++ Routines/Advanced/Sniper/Marksmanship.cs
@@ -32,7 +32,7 @@ namespace pCombat.Routines
 					Spell.Buff("Escape", ret => Me.IsStunned),
 					Spell.Buff("Shield Probe", ret => Me.HealthPercent <= 70),
 					Spell.Buff("Evasion", ret => Me.HealthPercent <= 30),
-					Spell.Buff("Adrenaline Probe", ret => Me.EnergyPercent <= 40),
+					Spell.Buff("Adrenaline Probe", ret => Me.EnergyPercent < 50),
 					Spell.Buff("Sniper Volley", ret => Me.EnergyPercent <= 60),
 					Spell.Buff("Entrench", ret => Me.CurrentTarget.StrongOrGreater() && Me.IsInCover()),
 					Spell.Buff("Laze Target"),
@@ -57,14 +57,25 @@ namespace pCombat.Routines
 
 					//Rotation
 					Spell.Cast("Distraction", ret => Me.CurrentTarget.IsCasting),
-					Spell.Cast("Followthrough"),
-					Spell.Cast("Penetrating Blasts", ret => Me.Level >= 26),
-					Spell.Cast("Series of Shots", ret => Me.Level < 26),
-					//Spell.DoT("Corrosive Dart", "Corrosive Dart"),
-					Spell.Cast("Ambush", ret => Me.BuffCount("Zeroing Shots") == 2),
-					Spell.Cast("Takedown", ret => Me.CurrentTarget.HealthPercent <= 30),
-					Spell.Cast("Snipe")
-					);
+                    //1. “Laze Target + Ambush > Followthrough”
+                    Spell.Cast("Followthrough"),
+                    Spell.Cast("Ambush"),
+                    //2. Suppressive Fire on 3 + targets > Corrosive Dart(The target does not the “Marked Target” debuff) > Ambush
+                    Spell.DoT("Corrosive Dart", "Corrosive Dart"),
+                    Spell.Cast("Ambush", ret => Me.BuffCount("Zeroing Shots") == 2),
+                    //3. “Penetrating Blasts > Followthrough”
+                    Spell.Cast("Penetrating Blasts", ret => Me.Level >= 26),
+                    Spell.Cast("Series of Shots", ret => Me.Level < 26),
+                    //4. “Snipe > Snipe > Followthrough(20 % Damage Increased)”
+                    Spell.Cast("Snipe"),
+                    //5. Takedown
+                    Spell.Cast("Takedown", ret => Me.CurrentTarget.HealthPercent <= 30)
+                    //6. Corrosive Dart(The target does have the “Marked Target” debuff)
+                    //7. Followthrough
+                    //8. Snipe > Corrosive Dart(Dart should only be applied to targets that don’t have the “Marked Target” debuff or targets that have over 200k HP for burst scenarios. It is generally better to precast this and just hope you get an 8k + Snipe which with 3 stacks of Honed Shots is like 2 / 3 chance.
+                    //9. Rifle Shot
+                   )
+                    ;
 			}
 		}
 
